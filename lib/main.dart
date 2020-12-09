b4dd6b5398f68c8fce0c92ac23754d29aa200f41
Darrshana R <67387068+Darrshana@users.noreply.github.com>
2020-12-10 04:05:38 +0530

    App bar text font style changed and sound file added to the ball button.
    Theme set to dark

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,25 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:audioplayers/audio_cache.dart';
+
 void main() => runApp(MaterialApp(
+  theme: ThemeData.dark(),
       home: BallPage(),
     ));
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
         backgroundColor: Colors.indigo[600],
         appBar: AppBar(
-        title: Center(child: Text('Ask Me Anything',style: TextStyle(
+        title: Center(child: Text('Ask Me Anything',
+            style: TextStyle(
             fontFamily: 'Lobster',
+            fontSize: 25.0,
             color: Colors.white,
-        fontWeight:FontWeight.bold)),),
+            fontWeight:FontWeight.bold)),),
     backgroundColor: Colors.grey[850],
     ),
     body: Ball(),);
@@ -39,6 +45,8 @@ class _BallState extends State<Ball> {
                 setState(() {
                   imgNum=Random().nextInt(5) + 1;
                 });
+                final player=AudioCache();
+                player.play('note7.wav');
                 },
                 child: Image.asset('images/ball$imgNum.png'),
               ),
---
+++ lib/welcome.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo[600],
+    );
+  }}
