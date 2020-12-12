0b9e6ee6fff066eb2ed459f7f0fab4372883039c
Darrshana R <67387068+Darrshana@users.noreply.github.com>
2020-12-12 21:39:14 +0530

    90 percent completed project

---
+++ lib/ball.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:animate_do/animate_do.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      backgroundColor: Colors.indigo[600],
+      appBar: AppBar(
+        toolbarHeight: 75.0,
+        title:  WavyAnimatedTextKit(text:['Ask Me Anything'],
+
+
+            isRepeatingAnimation: true,
+            textStyle: TextStyle(
+                fontFamily: 'Lobster',
+                fontSize: 30.0,
+                color: Colors.white,
+                fontWeight:FontWeight.bold)),
+        backgroundColor: Colors.grey[850],
+      ),
+      body: Ball(),);
+  }}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> with TickerProviderStateMixin{
+  int imgNum=1;
+  AnimationController animateController;
+  bool animate =false;
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child:
+        Center(child: FlatButton(
+            onPressed: (){
+
+              setState(() {
+                imgNum=Random().nextInt(5) + 1;
+                animate=true;
+
+              });
+
+              final player=AudioCache();
+              player.play('note7.wav');
+            },
+
+            child:Image.asset('images/ball$imgNum.png')),
+        ),
+
+      ),
+    );
+
+
+
+  }
+}
--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,10 +1,68 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:animatedroute/animatedroute.dart';
+import  'package:flutter/src/painting/gradient.dart';
+import 'ball.dart';
 class WelcomePage extends StatelessWidget {
+  static const String id = 'welcome';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.indigo[600],
+      backgroundColor: Colors.grey[850],
+      body: Welcome(),
     );
-  }}
+  }
+}
+
+class Welcome extends StatefulWidget {
+  @override
+  _WelcomeState createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> with TickerProviderStateMixin{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+
+          child:Center(
+            child:Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            Center(
+                child: ColorizeAnimatedTextKit(
+              text: ['Magic 8 Ball'],
+              textStyle: TextStyle(fontSize: 70.0, fontFamily: "Lobster"),
+              colors: [
+                Colors.purple,
+                Colors.blue,
+                Colors.yellow,
+                Colors.red,
+              ],
+             repeatForever: true,
+            )),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TypewriterAnimatedTextKit(
+                text:['Unable to decide?Need answer for a tricky question?','Just have the question in mind and press the button.',
+                  'Trust the ball!!'],textStyle: TextStyle(
+                  fontSize: 25.0,
+                  fontFamily: "Lobster"
+              ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top:50.0),
+              child: FloatingActionButton(child:Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('GO'),
+              ),tooltip: 'GO',foregroundColor: Colors.black,backgroundColor:Colors.indigo[600],onPressed: (){
+                //Navigator.pushNamed(context,'ball');
+                Navigator.of(context).push(AnimatedRoute(widget:BallPage(), curves: Curves.easeInOutCubic, alignment: Alignment.bottomCenter));
+              },),
+            )
+    ]),
+            ),
+           );
+
+  }
+}
